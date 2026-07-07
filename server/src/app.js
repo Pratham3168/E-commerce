002836a28a2d7f02225f9b2f23e420b9303e6691
Pratham3168 <prathamtanwar593@gmail.com>
2026-07-07 10:52:39 +0530

    Refactor middleware order in app.js for improved clarity

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
