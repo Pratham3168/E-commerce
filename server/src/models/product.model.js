fefaf3d7c75f74ad42eac92661592919ae5e2431
Pratham3168 <prathamtanwar593@gmail.com>
2026-07-10 04:28:07 +0530

    Add product model; define schema with validation, pre-save slug generation, and timestamps

---
+++ server/src/models/product.model.js
@@ -0,0 +1,107 @@
+import mongoose from 'mongoose';
+import slugify from 'slugify';
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    slug:{
+        type: String,
+        trim: true,
+        unique: true,
+        lowercase: true
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+    discountPercentage:{
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 100
+    },
+    stock:{
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    images: [
+    {
+        public_id: {
+            type: String,
+            default: ""
+        },
+        url: {
+            type: String,
+            default: ""
+        }
+    }
+],
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    isActive:{
+        type: Boolean,
+        default: true
+    },
+    averageRating:{
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5
+    },
+    totalReviews:{
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    soldCount:{
+        type: Number,
+        default: 0,
+        min: 0
+    },
+    sku: {
+    type: String,
+    unique: true,
+    required: true
+}
+}, {timestamps: true});
+
+
+productSchema.pre("save", function(next) {
+
+    if (!this.isModified("name")) {
+        return next();
+    }
+
+    this.slug = slugify(this.name, {
+        lower: true,
+        strict: true
+    });
+
+    next();
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
